5bb216a8c3179292590da2d13c4f330eaebd1d7b
tianlixin <newcitysoft@163.com>
2018-09-29 11:13:39 +0800

    实现使用mapper代理的方式增、删、改、查；增加Junit测试。

--- src/main/java/com/newcitysoft/study/mybatis/BlogMapper.java
+++ src/main/java/com/newcitysoft/study/mybatis/BlogMapper.java
@@ -2,6 +2,8 @@ package com.newcitysoft.study.mybatis;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * @author lixin.tian@renren-inc.com
  * @date 2018/9/18 14:19
@@ -10,4 +12,12 @@ import org.apache.ibatis.annotations.Mapper;
 public interface BlogMapper {
 
     Blog selectBlog(int id);
+
+    List<Blog> getAll();
+
+    void insert(Blog blog);
+
+    void update(Blog blog);
+
+    void deleteById(Integer id);
 }
---
+++ src/test/java/com/newcitysoft/test/BlogMapperTest.java
@@ -0,0 +1,101 @@
+package com.newcitysoft.test;
+
+import com.newcitysoft.study.mybatis.Blog;
+import com.newcitysoft.study.mybatis.BlogMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @author lixin.tian@renren-inc.com
+ * @date 2018/9/29 10:57
+ */
+public class BlogMapperTest {
+
+    private SqlSession sqlSession;
+
+    @Before
+    public void doBefore() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory =
+                new SqlSessionFactoryBuilder().build(inputStream);
+
+        sqlSession = sqlSessionFactory.openSession();
+    }
+
+    @Test
+    public void testGetAll() {
+        try {
+            BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+            List<Blog> blogList = mapper.getAll();
+            System.out.println(blogList);
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testSelect() {
+        try {
+            BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+            Blog blog = mapper.selectBlog(1);
+            System.out.println(blog);
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testInsert() {
+        Blog blog = new Blog();
+
+        blog.setTitle("标题一");
+        blog.setContent("内容...");
+        try {
+            BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+            mapper.insert(blog);
+
+            sqlSession.commit();
+        } finally {
+            sqlSession.close();
+        }
+
+    }
+
+    @Test
+    public void testUpdate() {
+        Blog blog = new Blog();
+
+        blog.setId(4);
+        blog.setTitle("标题一");
+        blog.setContent("啊实打实大安全去请问恶趣味 其味无穷二无群二群翁群");
+        try {
+            BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+            mapper.update(blog);
+
+            sqlSession.commit();
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    @Test
+    public void testDelete() {
+        try {
+            BlogMapper mapper = sqlSession.getMapper(BlogMapper.class);
+            mapper.deleteById(2);
+
+            sqlSession.commit();
+        } finally {
+            sqlSession.close();
+        }
+    }
+}
